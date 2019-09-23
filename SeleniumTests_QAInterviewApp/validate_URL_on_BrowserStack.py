87447eca59588eb21040bbc1e36427300aad6048
PreedhiVivek <preedhivivek@gmail.com>
2019-09-23 13:29:16 +0530

    Selenium URL validation test to run on browserstack

--- SeleniumTests_QAInterviewApp/validate_URL_on_BrowserStack.py
+++ SeleniumTests_QAInterviewApp/validate_URL_on_BrowserStack.py
@@ -14,7 +14,7 @@ SCOPE:
 
 import unittest, time
 from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
  
 class SeleniumOnBrowserStack(unittest.TestCase):
@@ -22,23 +22,17 @@ class SeleniumOnBrowserStack(unittest.TestCase):
     def setUp(self):
         desired_cap = { 'device': 'iPhone 7','realMobile': 'true', 'platform': 'iOS','browserName': 'safari', 'browserstack.debug': 'true' }
         self.driver = webdriver.Remote(command_executor='http://preedhivivek1:ASBEzbfpjskzSppnrHxJ@hub.browserstack.com:80/wd/hub',desired_capabilities=desired_cap)
- 
-    def test_QA_Interview_App_Test(self):
-        # Navigate to QA Interview (Factorial Calculator) application webpage
-        self.driver.get('https://qainterview.pythonanywhere.com/')
 
-        #Wait for the page to load and get displayed for verification
+    def test_QA_Interview_App(self):
+        #Go to the URL 
+        self.driver.get('https://qainterview.pythonanywhere.com/')
+        # Assert that the Page title is coorect
+        self.assertIn("Factoriall", self.driver.title)
         time.sleep(5)
-        #await asyncio.sleep(5)
-        # Check if the title of the page is proper
-        if(self.driver.title=="Factoriall"):
-            print(self.driver.title)
-            print ("Success: Factorial Calculator page launched successfully!")
-        else:
-            print ("Failed: Factorial Calculator page title is incorrect!") 
-        time.sleep(2)
-        #await asyncio.sleep(2)
- 
+        # Print the title of sign up page
+        print (self.driver.title)
+
+
     def tearDown(self):
         # Quit the browser window
         self.driver.quit()
